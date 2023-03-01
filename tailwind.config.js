da069ee9dfc036f971dcabe9c4c684a987903778
paaneko <paamayim.n3kudotayim@gmail.com>
2023-03-01 13:59:06 +0200

    Make some project preparations
    - add short paths
    - change gitingore
    - add project colors and change container size in tailwind
    - add some unnecessary files

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,24 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: 0,
+      screens: {
+        sm: '343px',
+        lg: '688px',
+        xl: '1170px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-poppins)'],
+      },
+    },
     colors: {
       'orange-main': '#FF5300',
-      'main-bg': '#ff49db',
+      'dark-white-bg': '#FFF7F4',
+      'main-bg': '#373737',
       'gray-pg': '#7B7B7B',
       'dark-bg': '#18191A',
       'dark-v-1': '#242526',
